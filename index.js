145618dd8b7e7d6a6ddabd63aa47024faee414e1
Prometheuse1 <zakariaehasnicerdelec@gmail.com>
2026-04-14 11:53:09 +0100

    js_final_commit_2

---
+++ index.js
@@ -0,0 +1,6 @@
+const toogle = document.getElementById("toogle");
+const navbar = document.getElementById("navbar");
+
+toogle.addEventListener("click", () => {
+    navbar.classList.toggle("apparait");
+});
